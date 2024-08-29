55ab190cb1db5e1c05e21df9f5318582bead9b48
Jongho Bae <jb4918@columbia.edu>
2024-08-29 15:06:01 -0400

    Refined header/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white  p-6 shadow-lg">
       <Header />
-      <main className="bg-gray-900 text-white min-h-screen p-6 shadow-lg">
+      <main className="flex-grow">
         <Outlet />
       </main>
       <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-900 text-white py-4">
+      <div className="container mx-auto flex justify-center space-x-8">
+        <a
+          href="https://github.com/jonghoBae0218"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:opacity-75"
+        >
+          <img
+            src="/images/github-mark-white.svg"
+            alt="Github Logo"
+            className="w-12 h-12"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jongho-bae-774645303/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:opacity-75"
+        >
+          <img
+            src="/images/linkedin.png"
+            alt="Linkedin Logo"
+            className="w-12 h-12"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/jongh0_bae/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:opacity-75"
+        >
+          <img
+            src="/images/instagram.png"
+            alt="Instagram Logo"
+            className="w-12 h-12"
+          />
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
