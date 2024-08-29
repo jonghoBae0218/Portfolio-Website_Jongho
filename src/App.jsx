11bf90c1e754a62584c14da6c9962ddc5529df63
Jongho Bae <jb4918@columbia.edu>
2024-08-28 21:27:59 -0400

    Added router and base page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-
-// src/App.jsx
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="bg-gray-800 text-white p-8 min-h-screen flex items-center justify-center">
-      <div className="bg-gray-700 p-6 rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold mb-4">Tailwind CSS is working!</h1>
-        <p className="text-lg">This is a test of Tailwind utility classes.</p>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded mt-4 hover:bg-blue-600">
-          Test Button
-        </button>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,37 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import AboutMe from "./pages/AboutMe.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      // {
+      //   path: "/portfolio",
+      //   element: <Portfolio />,
+      // },
+      // {
+      //   path: "/contact",
+      //   element: <Contact />,
+      // },
+      // {
+      //   path: "/resume",
+      //   element: <Resume />,
+      // },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
